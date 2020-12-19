f2910c3f35fd62c35ea5d364e8b762cf5f2073db
Natalie Japaridze <Jeffersoon22@users.noreply.github.com>
2020-12-19 15:35:42 +0400

    Started shipping form

--- src/components/container/ShippingInfo/ShippingInfo.jsx
+++ src/components/container/ShippingInfo/ShippingInfo.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import Form from "../../presentation/Form/Form";
+import TextField from '@material-ui/core/TextField';
+import styles from "./ShippingInfo.module.css"
+
+import Shipping from "../../../assets/img/shipping.png"
 
 
 function ShippingInfo() {
-  
-  return <p>Shipping</p>
+
+  return (
+    <div className={styles["wrapper"]}>
+      <div>
+        <img src={Shipping} alt="Shipping"/>
+      </div>
+      <Form >
+        <div>
+          <TextField id="outlined-basic" label="First Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Address Line 1" variant="outlined" />
+          <TextField id="outlined-basic" label="Address Line 2" variant="outlined" />
+          <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+        </div>
+      </Form>
+    </div>
+
+  )
 }
 
 
